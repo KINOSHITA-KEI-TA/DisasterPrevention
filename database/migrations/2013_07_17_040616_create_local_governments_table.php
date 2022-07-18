d503a9515792ff2cad2fd39d650071c5116416ba
ShuheiOhno <inu.suika8@gmail.com>
2022-07-18 13:27:50 +0900

    modify migrationfile
    user
    local government

--- database/migrations/2013_07_17_040616_create_local_governments_table.php
+++ database/migrations/2013_07_17_040616_create_local_governments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('local_governments', function (Blueprint $table) {
             $table->id();
+            $table->integer("local_government_code");
             $table->string("name");
             $table->timestamps();
         });
