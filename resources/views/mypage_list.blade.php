a7931b537da599c442cea5e266845e43aa4ae537
きのしたけいた <67889594+KINOSHITA-KEI-TA@users.noreply.github.com>
2023-02-13 21:03:58 +0900

    Create mypage_list.blade.php

---
+++ resources/views/mypage_list.blade.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>DisasterPrevention</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+    <meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+    <meta name="author" content="FreeHTML5.co" />
+
+
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+    
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="css/flexslider.css">
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+
+</head>
+    <body>
+    <div id="fh5co-page">
+        <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+        @extends('common.aside')
+
+        <div id="fh5co-main">
+            <div class="fh5co-narrow-content">
+                <h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">バディリスト</h2>
+                <h3 class="BuddyListTitle">木下啓太</h3>    
+                <div class="BuddyList">
+                    <div class="BuddyListContent">
+                        <div class="BuddyListContentImage"><img src="images/23880633_s.jpeg" width="50px" height="50px" alt="カテゴリー画像" class="BuddyListContentImg"></div>
+                        <div class="BuddyListContentName">
+                        <p class="BuddyListName">バディAさん</p>
+                        <p class="BuddyListName">埼玉県 草加市</p>
+                        </div>
+                        <div class="BuddyListName_btn_Member_box">
+                        <button class="BuddyListName BuddyListName_btn_Member" id="btn">バディ解除</button>
+                        </div>
+                    </div>
+                    <div class="BuddyListContent">
+                        <div class="BuddyListContentImage"><img src="images/23880633_s.jpeg" width="50px" height="50px" alt="カテゴリー画像" class="BuddyListContentImg"></div>
+                        <div class="BuddyListContentName">
+                        <p class="BuddyListName">バディBさん</p>
+                        <p class="BuddyListName">埼玉県 越谷市</p>
+                        </div>
+                        <div class="BuddyListName_btn_Member_box">
+                        <button class="BuddyListName BuddyListName_btn_Member" id="btn">バディ解除</button>
+                        </div>
+                    </div>
+                    <div class="BuddyListContent">
+                        <div class="BuddyListContentImage"><img src="images/23880633_s.jpeg" width="50px" height="50px" alt="カテゴリー画像" class="BuddyListContentImg"></div>
+                        <div class="BuddyListContentName">
+                        <p class="BuddyListName">バディCさん</p>
+                        <p class="BuddyListName">埼玉県 蕨市</p>
+                        </div>
+                        <div class="BuddyListName_btn_Member_box">
+                        <button class="BuddyListName BuddyListName_btn_Member">バディ解除</button>
+                        </div>
+                    </div>
+                    <div class="BuddyListContent">
+                        <div class="BuddyListContentImage"><img src="images/23880633_s.jpeg" width="50px" height="50px" alt="カテゴリー画像" class="BuddyListContentImg"></div>
+                        <div class="BuddyListContentName">
+                        <p class="BuddyListName">バディCさん</p>
+                        <p class="BuddyListName">埼玉県 蕨市</p>
+                        </div>
+                        <div class="BuddyListName_btn_Member_box">
+                        <button class="BuddyListName BuddyListName_btn_Member">バディ解除</button>
+                        </div>
+                    </div>
+                    <div class="BuddyListContent">
+                        <div class="BuddyListContentImage"><img src="images/23880633_s.jpeg" width="50px" height="50px" alt="カテゴリー画像" class="BuddyListContentImg"></div>
+                        <div class="BuddyListContentName">
+                        <p class="BuddyListName">バディCさん</p>
+                        <p class="BuddyListName">埼玉県 蕨市</p>
+                        </div>
+                        <div class="BuddyListName_btn_Member_box">
+                        <button class="BuddyListName BuddyListName_btn_Member">バディ解除</button>
+                        </div>
+                    </div>
+                    <div class="BuddyListContent">
+                        <div class="BuddyListContentImage"><img src="images/23880633_s.jpeg" width="50px" height="50px" alt="カテゴリー画像" class="BuddyListContentImg"></div>
+                        <div class="BuddyListContentName">
+                        <p class="BuddyListName">バディCさん</p>
+                        <p class="BuddyListName">埼玉県 蕨市</p>
+                        </div>
+                        <div class="BuddyListName_btn_Member_box">
+                        <button class="BuddyListName BuddyListName_btn_Member">バディ解除</button>
+                        </div>
+                    </div>
+                    <div class="BuddyListContent">
+                        <div class="BuddyListContentImage"><img src="images/23880633_s.jpeg" width="50px" height="50px" alt="カテゴリー画像" class="BuddyListContentImg"></div>
+                        <div class="BuddyListContentName">
+                        <p class="BuddyListName">バディCさん</p>
+                        <p class="BuddyListName">埼玉県 蕨市</p>
+                        </div>
+                        <div class="BuddyListName_btn_Member_box">
+                        <button class="BuddyListName BuddyListName_btn_Member">バディ解除</button>
+                        </div>
+                    </div>
+                    <div class="BuddyListContent">
+                        <div class="BuddyListContentImage"><img src="images/23880633_s.jpeg" width="50px" height="50px" alt="カテゴリー画像" class="BuddyListContentImg"></div>
+                        <div class="BuddyListContentName">
+                        <p class="BuddyListName">バディCさん</p>
+                        <p class="BuddyListName">埼玉県 蕨市</p>
+                        </div>
+                        <div class="BuddyListName_btn_Member_box">
+                        <button class="BuddyListName BuddyListName_btn_Member">バディ解除</button>
+                        </div>
+                    </div>
+                    </div>
+            <div id="get-in-touch">
+                <div class="fh5co-narrow-content">
+                    <div class="row">
+                        <div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+                            <h1 class="fh5co-heading-colored">お問合せはこちら</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                            <p class="fh5co-lead"></p>
+                            <p><a href="/contact" class="btn btn-primary">お問合せ</a></p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- Flexslider -->
+    <script src="js/jquery.flexslider-min.js"></script>
+    
+    
+    <!-- MAIN JS -->
+    <script src="js/main.js"></script>
+    <script>
+        document.getElementById("btn").onclick = function() {
+  let checkSaveFlg = window.confirm('保存を実行してもよろしいですか？');
+  
+  if(checkSaveFlg) {
+    document.getElementById("saveResult").textContent = "保存を実行しました。";
+  } else {
+    document.getElementById("saveResult").textContent = "保存をキャンセルしました。";
+  }
+};
+// var btn = document.getElementsByClassName('BuddyListName_btn_Member');
+
+// btn.addEventListener('click', function() {
+//     var result = window.confirm('バディ解除してもよろしいですか？');
+    
+//     if( result ) {
+//         console.log('OKがクリックされました');
+//     }
+//     else {
+//         console.log('キャンセルがクリックされました');
+//     }
+// })
+</script>
+    </body>
+</html>
