df1fa1beaec805cf178711db227f078fda24a9e8
きのしたけいた <67889594+KINOSHITA-KEI-TA@users.noreply.github.com>
2023-02-13 20:59:48 +0900

    mypage

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,188 +1,268 @@
-
 <!DOCTYPE html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>DisasterPrevention</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FreeHTML5.co" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>DisasterPrevention</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+    <meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+    <meta name="author" content="FreeHTML5.co" />
 
 
-	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
 
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico">
 
-	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
-	
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- Flexslider  -->
-	<link rel="stylesheet" href="css/flexslider.css">
-	<!-- Theme style  -->
-	<link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+    
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="css/flexslider.css">
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css">
 
-	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
 
 </head>
-	<body>
-	<div id="fh5co-page">
-		<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-		@extends('common.aside')
+    <body>
+    <div id="fh5co-page">
+        <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+        @extends('common.aside')
 
-		<div id="fh5co-main">
-			<div class="fh5co-narrow-content">
-				<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Portfolio</h2>
-				<div class="row row-bottom-padded-md">
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-1.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Illustration</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-2.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Brading</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-3.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Illustration</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-4.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Illustration</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-5.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Brading</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-6.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Illustration</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-1.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Illustration</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-2.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Brading</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-1.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Illustration</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-2.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Brading</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-3.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Illustration</span>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3 col-sm-6 col-padding text-center animate-box">
-						<a href="#" class="work image-popup" style="background-image: url(images/img-4.jpg);">
-							<div class="desc">
-								<h3>Project Name</h3>
-								<span>Illustration</span>
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		
-			<div id="get-in-touch">
-				<div class="fh5co-narrow-content">
-					<div class="row">
-						<div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-							<h1 class="fh5co-heading-colored">お問合せはこちら</h1>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-							<p class="fh5co-lead"></p>
-							<p><a href="/contact" class="btn btn-primary">お問合せ</a></p>
-						</div>
-						
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Flexslider -->
-	<script src="js/jquery.flexslider-min.js"></script>
-	
-	
-	<!-- MAIN JS -->
-	<script src="js/main.js"></script>
-
-	</body>
+        <div id="fh5co-main">
+            <div class="fh5co-narrow-content">
+                <h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">マイページ</h2>
+                <p><a href="#">プロフィール編集</a></p>
+                <!-- <div class="row row-bottom-padded-md">
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-1.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Illustration</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-2.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Brading</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-3.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Illustration</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-4.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Illustration</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-5.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Brading</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-6.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Illustration</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-1.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Illustration</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-2.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Brading</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-1.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Illustration</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-2.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Brading</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-3.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Illustration</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-padding text-center animate-box">
+                        <a href="#" class="work image-popup" style="background-image: url(images/img-4.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Illustration</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div> -->
+            <div class="row row-bottom-padded-md MyProfile">
+                <div class="animate-box animated col-md-3 col-padding col-sm-6 fadeInUp text-center">
+                    <a href="#" class="work image-popup" style="background-image: url(images/img-1.jpg);">
+                    <div class="desc">
+                    <h3>プロフィール画像</h3>       
+                    <span>Illustration</span>
+                    </div>
+                    </a>
+                    </div>
+                    <div class="MyList">
+                    <h2 class="MyListTitle">名前</h2>
+                    <p class="MyListContent">木下啓太</p>
+                    <h2 class="MyListTitle">ニックネーム</h2>
+                    <p class="MyListContent">オールマイト</p>
+                    <h2 class="MyListTitle">出身地</h2>
+                    <div class="Area">
+                        <p class="MyListContent">東京都</p>
+                        <p class="MyListContent">&nbsp葛飾区</p>
+                    </div>
+                    <h2 class="MyListTitle">住まい地域</h2>
+                    <div class="Area">
+                        <p class="MyListContent">埼玉県</p>
+                        <p class="MyListContent">&nbsp草加市</p>
+                    </div>
+                    <h2 class="MyListTitle">所属カテゴリー</h2>
+                    <div class="MyCategory-list">
+                    <div class="support-box">
+                        <div class="support-img"><img src="images/23880633_s.jpeg" width="200px" height="200px" alt="カテゴリー画像"></div>
+                        <div class="support-img"></div>
+                        <div class="support-img"></div>
+                        <div class="support-img"></div>
+                        <div class="support-img"></div>
+                        <div class="support-img"></div>
+                        <div class="support-img"></div>
+                        <div class="support-img"></div>
+                        <div class="support-img"></div>
+                    </div>
+                    </div>
+                    <!-- <ul>
+                        <li>
+                            <div class="support-img">
+                            <a href="#" class="support-img_1" style="background-image: url(images/img-2.jpg);">
+                            <div class="desc">
+                                <h3>Project Name</h3>
+                                <span>Brading</span>
+                            </div>
+                            </a>
+                            </div>
+                        </li>
+                        <li>〜</li>
+                        <li>〜</li>
+                    </ul> -->
+                    <h2 class="MyListTitle">バディ</h2>
+                    <p class="MyListContent" id="ListTitle">23</p>
+                    <!-- <label for="toggle" onclick=""  for="menuToggle">もっと見る</label>  
+                    <input type="checkbox" id="toggle" autocomplete="off"> -->
+                    <input type="checkbox" id="toggle" autocomplete="off">
+                    <ul id="menu">  
+                      <li class="MypageBuddyList">バディA</li>
+                      <li class="MypageBuddyList">バディB</li>
+                      <li class="MypageBuddyList">バディC</li>
+                      <li class="MypageBuddyList ListMove"><a href="/mypage_list">バディリスト</a></li>
+                    </ul>
+                    <label for="toggle" onclick=""  for="menuToggle">もっと見る</label>  
+                </div>
+            <button class="fixed_btn">バディになる！</button>
+            <button class="fixed_btn_Application">バディ申請中</button>
+            <button class="fixed_btn_Member">バディ解除</button>
+            <center>
+            <a href="#modal-01" class=""><button class="fixed_btn_question modal-button">？</button></a>
+            </center>
+            <div class="modal-wrapper" id="modal-01">
+            <a href="#!" class="modal-overlay"></a>
+            <div class="modal-window">
+            <div class="modal-content">
+            <p class="modal_title">防サイトーク内のバディとは？</p>
+            <p>離れた地域での助け合いや防災に関する情報交換を行える関係です。</p>
+            <p>出会いは不適切な言動は直ちに退会とさせていただきます。</p>
+            </div>
+            <a href="#!" class="modal-close"><i class="far fa-times-circle"></i></a>
+            </div>
+            </div>
+            </div>
+        
+            <div id="get-in-touch">
+                <div class="fh5co-narrow-content">
+                    <div class="row">
+                        <div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+                            <h1 class="fh5co-heading-colored">お問合せはこちら</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                            <p class="fh5co-lead"></p>
+                            <p><a href="/contact" class="btn btn-primary">お問合せ</a></p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <!-- Flexslider -->
+    <script src="js/jquery.flexslider-min.js"></script>
+    
+    
+    <!-- MAIN JS -->
+    <script src="js/main.js"></script>
+    </body>
 </html>
-
