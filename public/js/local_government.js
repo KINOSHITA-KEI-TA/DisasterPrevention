1db576ceb6353457a0c302041cf21bafc59993fc
ShuheiOhno <inu.suika8@gmail.com>
2023-05-02 00:00:09 +0900

    入力、イベント確認

--- public/js/local_government.js
+++ public/js/local_government.js
@@ -1,1 +1,8 @@
 // alert('aaa')
+const keyword = document.getElementById('keyword');
+
+keyword.addEventListener('input', (e) => {
+    console.log('e', e)
+})
+
+console.log('keyword', keyword)
--- resources/views/add_user_info.blade.php
+++ resources/views/add_user_info.blade.php
@@ -49,15 +49,9 @@ add_user_info
 
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('検索') }}</label>
-
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="keyword" type="text" class="form-control">
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
                         {{-- ここまで --}}
