beae4acf935ed97a6673ad783c114d9e9475a689
ShuheiOhno <inu.suika8@gmail.com>
2023-05-02 09:48:12 +0900

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,6 @@ class RegisterController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-    // protected $redirectTo = RoteServiceProvider::LOCAL_GOVERNMENT;
 
     /**
      * Create a new controller instance.
